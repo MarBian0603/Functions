e3d8d110a9ffe51ccb4f4e4a66d0db3dc9f6a603
MarBian0603 <roccimail9721@gmail.com>
2024-06-18 14:06:24 +0200

    Update compute_pq.m

--- compute_pq.m
+++ compute_pq.m
@@ -1,13 +1,15 @@
-function [p,q]=compute_pq(RR,f)
-%RR is the signal
-%f is the frequency
-N=length(RR);
+function [p,q]=compute_pq(phi,f)
+%This function computes the vector p and the vector q.
+%The function takes as inputs:
+%phi - the signal
+%f - the frequency
+N=length(phi);
 p=zeros(1,N);
 q=zeros(1,N);
-p(1)=RR(1)*cos(f);
-q(1)=RR(1)*sin(f);
+p(1)=phi(1)*cos(f);
+q(1)=phi(1)*sin(f);
 for i=2:N
-    p(i)=p(i-1)+RR(i)*cos(f*i);
-    q(i)=q(i-1)+RR(i)*sin(f*i);
+    p(i)=p(i-1)+phi(i)*cos(f*i);
+    q(i)=q(i-1)+phi(i)*sin(f*i);
 end
 end
